a2f8375c5fbaf9c62c247a7d74da8038b8a54b99
YOUNG <young@client-181.research.netlab.hut.fi>
2017-09-07 20:18:18 +0300

    Fix file target membership

--- ARMeasure/Models/RealmObjects.swift
+++ ARMeasure/Models/RealmObjects.swift
@@ -11,11 +11,11 @@ import RealmSwift
 /**
  The highest hierarchy in DB which contains all data
  */
-final public class MeasureSessionList: Object {
+final class MeasureSessionList: Object {
     @objc dynamic var id = ""
     let sessions = List<MeasureSession>()
     
-    override static public func primaryKey() -> String? {
+    override static func primaryKey() -> String? {
         return "id"
     }
 }
@@ -25,32 +25,32 @@ final public class MeasureSessionList: Object {
  One session starts when user starts the app and ends when user exits
  the app
  */
-final public class MeasureSession: Object {
+final class MeasureSession: Object {
     @objc dynamic var id = ""
     let datum = List<MeasureData>()
     
-    override static public func primaryKey() -> String? {
+    override static func primaryKey() -> String? {
         return "id"
     }
 }
 
-final public class MeasureData: Object {
+final class MeasureData: Object {
     @objc dynamic var screenshotName = ""
     let worldCoordinates = List<Coordinates3D>()
     let screenCoordinates = List<Coordinates2D>()
     
-    override static public func primaryKey() -> String? {
+    override static func primaryKey() -> String? {
         return "screenshotName"
     }
 }
 
-final public class Coordinates3D: Object {
+final class Coordinates3D: Object {
     @objc dynamic var x: Float = 0.0
     @objc dynamic var y: Float = 0.0
     @objc dynamic var z: Float = 0.0
 }
 
-final public class Coordinates2D: Object {
+final class Coordinates2D: Object {
     @objc dynamic var x: Float = 0.0
     @objc dynamic var y: Float = 0.0
 }
